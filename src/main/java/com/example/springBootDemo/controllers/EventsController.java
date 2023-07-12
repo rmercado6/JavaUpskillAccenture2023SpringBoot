0fe9787f9fd824dcc1660e6922cacb59bb72f596
Mercado <ricardo.mercado@accenture.com>
2023-07-12 12:54:20 -0600

    initial commit

---
+++ src/main/java/com/example/springBootDemo/controllers/EventsController.java
@@ -0,0 +1,9 @@
+package com.example.springBootDemo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("events")
+public class EventsController {
+}
